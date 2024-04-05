3c24aaab03258846f1a2d04a48d20cee5658ac66
Daniel Hall <beta@hai.haus>
2024-04-05 10:04:07 -0700

    Added extended object types

--- vocab/object.go
+++ vocab/object.go
@@ -2,6 +2,38 @@ package vocab
 
 // [Object] and [Link] types use [Object] or [Link] as their base.
 
+// Tombstone represents content that has been deleted.
+type Tombstone struct {
+  Object
+
+  // FormerType is the type of the object that was deleted.
+  // SEE: [https://www.w3.org/ns/activitystreams#formerType]
+  FormerType string `json:"formerType"`
+  
+  // Deleted is the datetimestamp of when the object was deleted. 
+  Deleted string `json:"deleted"`
+}
+
+// Relationship describes the relationship between two individuals.
+// SEE: [https://www.w3.org/ns/activitystreams#Relationship]
+type Relationship struct {
+  Object
+  
+  // Relationship is the type of the relationship
+  Relationship string `json:"relationship"`
+}
+
+// Profile is a content [Object] that describes another [Object], in most cases
+// it is an actor.
+// SEE: [https://www.w3.org/ns/activitystreams#Profile]
+type Profile struct {
+  Object
+
+  // Describes is the [Object] that the Profile describes.
+  // SEE: [https://www.w3.org/ns/activitystreams#describes].
+  Describes Object `json:"describes"`
+}
+
 // Place represents a location that can be physical or logical.
 // SEE: [https://www.w3.org/TR/activitystreams-vocabulary/#dfn-place]
 type Place struct {
